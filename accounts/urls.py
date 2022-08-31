2e3986b741c73d6dca242714400e7bed62ad2502
geekray <kirimikelvinkk@gmail.com>
2022-08-31 15:34:19 +0300

    final1 commit

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from ast import pattern
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('register',views.register, name="register"),
+    path('login',views.login, name="login"),
+    path('logout',views.logout, name="logout"),
+   
+]
